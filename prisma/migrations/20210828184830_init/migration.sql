b0ba9a5ff40d0601b84af703d925ea14af21c55f
alexandre.valadares <alexandre.@corenmg.gov.br>
2022-07-21 11:00:03 -0300

    Projeto final

--- prisma/migrations/20210828184830_init/migration.sql
+++ prisma/migrations/20210828184830_init/migration.sql
@@ -5,6 +5,6 @@ CREATE TABLE `User` (
     `password` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User.email_unique`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // Pipes
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +13,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
   await app.listen(3000);
 }
+
 bootstrap();
